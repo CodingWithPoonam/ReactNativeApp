7bf2ca0a650f816f3ccd058cf80408790f6a9eb9
harish8939 <66994085+harish8939@users.noreply.github.com>
2026-05-21 15:35:28 +0530

    View,Button,Text

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+  import React from 'react'
+  import {Button,Text,View} from 'react-native'
+
+  const App = () => {
+    return (
+  <View>
+    <Text style={ { fontSize : 30, } }>React Native Mobile App Developer</Text>
+    <Text style={ { fontSize : 30, } }>Poonam Joshi</Text>
+    <Button title='Press here'/>
+  </View>
+  )
+  }
+    
+  export default App
